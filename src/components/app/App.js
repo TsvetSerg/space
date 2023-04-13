e5f58447a9a8008d9e401c4168ca75c6f1feab6f
Sergay <tsvetkoffgolod@yandex.ru>
2023-04-13 17:08:39 +0300

    front

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,23 +1,55 @@
-import logo from '../../logo.svg';
 import './App.css';
+import back from '../../images/backss.png'
+import alllers from '../../images/alllers.svg'
+import articless from '../../images/articless.svg'
+import avoirss from '../../images/avoirss.svg'
+import conjugass from '../../images/conjugass.svg'
+import etress from '../../images/etress.svg'
+import fairess from '../../images/fairess.svg'
+import formess from '../../images/formess.svg'
+import genress from '../../images/genress.svg'
+import plurielss from '../../images/plurielss.svg'
+import adjectifss from '../../images/adjectifss.svg'
+import popup from '../popup/popup';
+import React from 'react';
+
+
+
+
+
 
 function App() {
+  const [isAddPopup, setAddPopup] = React.useState(false)
+
+
+  function open() {
+    setAddPopup(true)
+  }
+
+  function close() {
+    setAddPopup(false)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <img src={back} alt="" className='back' />
+
+      <div className='wrapper'>
+        <img className='place alllers' src={alllers} alt="" />
+        <img className='place articless' src={articless} alt="" />
+        <img className='place avoirss' src={avoirss} alt="" />
+        <img className='place conjugass' src={conjugass} alt="" />
+        <img className='place etress' src={etress} alt="" />
+        <img className='place fairess' src={fairess} alt="" />
+        <img className='place formess' src={formess} alt="" />
+        <img className='place genress' src={genress} alt="" />
+        <img className='place plurielss' src={plurielss} alt="" />
+        <img className='place adjectifss' src={adjectifss} alt="" />
+      </div>
+
+
     </div>
   );
 }
---
+++ src/components/popup/popup.js
@@ -0,0 +1,17 @@
+import './popup.css';
+
+
+function Popup(props) {
+
+
+
+  return (
+
+    <div className=''>
+      <h1>1hellow</h1>
+    </div>
+
+  )
+}
+
+export default Popup;
